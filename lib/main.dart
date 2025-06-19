c0174a9e28c128b5b4b933c3dfe15e45b4bee50e
neil761 <alvarezneilandrew486@gmail.com>
2025-06-19 10:19:37 +0800

    design

--- lib/main.dart
+++ lib/main.dart
@@ -7,34 +7,84 @@ void main() {
         title: Text('My First App'),
         backgroundColor: Colors.lightBlueAccent,
       ),
-      body: Center(
-          child: Column(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('Call of', style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontWeight: FontWeight.bold,
+                Text('Gun:', 
+                style: TextStyle(
                   fontSize: 30,
-                  color: Colors.indigo
-                ),),
-                Text('Duty', style: TextStyle(
-                    fontFamily: 'Times New Roman',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40,
-                    color: Colors.lightBlue
-                ),),
-                Text('Mobile', style: TextStyle(
-                    fontFamily: 'Courier New',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 50,
-                    color: Colors.black87
-                ),)
-              ]
-          )
-      )
-
-
-      )
-    )
-  );
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Times New Roman'
+                )),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  color: Colors.red,
+                  child:Text('AK117', 
+                  style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Times New Roman',
+                  color: Colors.white,
+                )),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Type:', 
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Times New Roman'
+                )),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  color: Colors.blue,
+                  child:Text('Assault Rifle', 
+                  style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Times New Roman',
+                  color: Colors.white,
+                )),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(50, 60, 70, 80),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Skin:', 
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Times New Roman'
+                )),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  color: Colors.green,
+                  child:Text('Mythic', 
+                  style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Times New Roman',
+                  color: Colors.white,
+                )),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ),
+  ));
 }
 
