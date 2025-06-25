d2442777e15afb07535630d195ab6af6683f90d7
neil761 <alvarezneilandrew486@gmail.com>
2025-06-25 09:35:03 +0800

    user

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,174 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('My First App'),
-        backgroundColor: Colors.lightBlueAccent,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Gun:', 
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Times New Roman'
-                )),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                  color: Colors.red,
-                  child:Text('AK117', 
-                  style: TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'Times New Roman',
-                  color: Colors.white,
-                )),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Type:', 
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Times New Roman'
-                )),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                  color: Colors.blue,
-                  child:Text('Assault Rifle', 
-                  style: TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'Times New Roman',
-                  color: Colors.white,
-                )),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(50, 60, 70, 80),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Skin:', 
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Times New Roman'
-                )),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                  color: Colors.green,
-                  child:Text('Mythic', 
-                  style: TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'Times New Roman',
-                  color: Colors.white,
-                )),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    ),
+    home: Profile()
   ));
 }
 
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Arsenal',
+                 style: TextStyle(
+                  color: Colors.white,
+                 ),),
+        backgroundColor: const Color.fromARGB(255, 75, 83, 32),
+      ),
+      body: Container(
+        margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+        child: Column(
+          children: [
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Text('User Profile',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30
+                    ),),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.account_circle_rounded, size: 100,)
+                ],
+              ),
+            ),
+            Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(30, 20, 0, 0),
+                  child: Row(
+                  children: [
+                    Container(
+                      child: Text('Name:',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),),
+                    ),
+                  ],
+                ),
+                ),
+                Container(
+                  child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                      child: Text('Neil Andrew T. Alvarez:'),
+                    ),
+                  ],
+                ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(30, 10, 0, 0),
+                  child: Row(
+                  children: [
+                    Container(
+                      child: Text('Email:',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),),
+                    ),
+                  ],
+                ),
+                ),
+                Container(
+                  child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                      child: Text('neilandrew03@gmail.com'),
+                    ),
+                  ],
+                ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(30, 10, 0, 0),
+                  child: Row(
+                  children: [
+                    Container(
+                      child: Text('Company:',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),),
+                    ),
+                  ],
+                ),
+                ),
+                Container(
+                  child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                      child: Text('Batangas State University TNEU Balayan'),
+                    ),
+                  ],
+                ),
+                ),
+                Container(
+                  child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(30, 10, 0, 0),
+                      child: Text('Contact Number:',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),),
+                    ),
+                  ],
+                ),
+                ),
+                Container(
+                  child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                      child: Text('09876543212'),
+                    ),
+                  ],
+                ),
+                ),
+                Container(
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 250, 0, 0),
+                        width: 300,
+                       child: ElevatedButton(onPressed: (){},
+                 child: Text('Logout',
+                 style: TextStyle(
+                  color: Colors.white,
+                 ),),
+                 style: ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(255, 75, 83, 32)),
+                )
+                        
+                      )
+                    ]
+                  )
+
+                 
+                 )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
